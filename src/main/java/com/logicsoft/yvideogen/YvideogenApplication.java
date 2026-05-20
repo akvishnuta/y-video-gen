f3b4e5d83e295867189d8c91dc855a6254402f25
akvishnuta <akhilkumarta1994@gmail.com>
2026-05-20 18:18:56 +0530

    initial commit

---
+++ src/main/java/com/logicsoft/yvideogen/YvideogenApplication.java
@@ -0,0 +1,13 @@
+package com.logicsoft.yvideogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YvideogenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YvideogenApplication.class, args);
+	}
+
+}
